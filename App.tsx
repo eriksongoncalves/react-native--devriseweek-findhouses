903a69bd9337f795e8290464c5c99e4ba386a307
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-03-26 11:44:53 -0300

    feat: config styled-components

--- App.tsx
+++ App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import { ThemeProvider } from 'styled-components';
+
+import theme from './src/styles/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
---
+++ README.md
@@ -0,0 +1,18 @@
+# <img height="24" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png" alt="React Native" title="React Native"/> DevRiseWeek - Find Houses
+
+<br />
+
+## Configurações iniciais
+  - Na raiz do projeto instale as dependências:
+
+  ```
+    $ yarn install
+  ```
+
+<br />
+
+## Rode a aplicação:
+
+```
+  $ yarn start
+```
